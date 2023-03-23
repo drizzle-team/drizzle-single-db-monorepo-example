9d50cabc10f5c45bfe2f52b126b5570799845bce
Alex Blokh <aleksandrblokh@gmail.com>
2023-03-23 22:23:01 +0200

    +

--- projects/planetscale-mysql2/drizzle/0000_far_sunspot.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `users` (
-	`id` int AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	`name` text NOT NULL,
-	`age` int,
-	`occupation` text
-);
--- projects/planetscale-sls/schema.ts
+++ projects/planetscale-sls/schema.ts
@@ -1,7 +1,5 @@
-import { mysqlTableCreator, text } from "drizzle-orm/mysql-core";
+import { mysqlTable, text } from "drizzle-orm/mysql-core";
 import { int } from "drizzle-orm/mysql-core";
-
-const mysqlTable = mysqlTableCreator((name) => `alex_${name}`);
 
 export const people = mysqlTable("people", {
   id: int("id").autoincrement().primaryKey(),
